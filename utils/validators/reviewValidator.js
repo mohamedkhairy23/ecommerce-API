928fb5963e9c9a962065c102d0495f773d61db5c
Mohamed Khairy <mohamedkhairy17696@gmail.com>
2024-08-13 02:33:32 +0300

    fix small bug in delete review validator

--- utils/validators/reviewValidator.js
+++ utils/validators/reviewValidator.js
@@ -55,26 +55,27 @@ exports.updateReviewValidator = [
 
   validatorMiddleware,
 ];
-
 exports.deleteReviewValidator = [
   check("id")
     .isMongoId()
-    .withMessage("Invalid review id format")
-    .custom((val, { req }) =>
-      Review.findById(val).then((review) => {
-        if (!review) {
-          return Promise.reject(
-            new Error(`There is no review with this id ${val}`)
-          );
-        }
-        // check if a logged in user own the review
-        if (review.user.toString() !== req.user._id.toString()) {
-          return Promise.reject(
-            new Error(`You are not allowed to perform this action`)
-          );
-        }
-      })
-    ),
-
+    .withMessage("Invalid Review id format")
+    .custom((val, { req }) => {
+      // check if a logged in user own the review
+      if (req.user.role === "user") {
+        return Review.findById(val).then((review) => {
+          if (!review) {
+            return Promise.reject(
+              new Error(`There is no review with id ${val}`)
+            );
+          }
+          if (review.user._id.toString() !== req.user._id.toString()) {
+            return Promise.reject(
+              new Error(`Your are not allowed to perform this action`)
+            );
+          }
+        });
+      }
+      return true;
+    }),
   validatorMiddleware,
 ];
