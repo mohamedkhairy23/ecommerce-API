a03d99dd17df5e42ea38743fe8679591b8fc9f5a
Mohamed Khairy <mohamedkhairy17696@gmail.com>
2024-08-17 23:04:59 +0300

    add endpoint description to webhook checkout function

--- services/orderService.js
+++ services/orderService.js
@@ -227,6 +227,9 @@ const createCardOrder = async (session) => {
   }
 };
 
+// @desc    This webhook will run when stripe payment success paid
+// @route   POST /api/v1/orders/webhook-checkout
+// @access  Protected/User
 exports.webhookCheckout = asyncHandler(async (req, res, next) => {
   const sig = req.headers["stripe-signature"];
 
